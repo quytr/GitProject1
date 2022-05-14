ab6cc070a35fd6c2f679793b324d4ae58d44ad4f
quytr <quy.truong310@gmail.com>
2022-05-14 12:03:49 -0400

    2nd commit

--- src/com/syntax/Main.java
+++ src/com/syntax/Main.java
@@ -5,5 +5,11 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("I am Iron Man");
+        System.out.println("I am Iron Man");
+        System.out.println("I am Iron Man");
+        System.out.println("I am Iron Man");
+        System.out.println("I am Iron Man");
+        
+
     }
 }
